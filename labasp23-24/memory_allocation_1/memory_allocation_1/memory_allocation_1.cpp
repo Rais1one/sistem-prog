bddd850cfbe43d73564c41f43b6799ef31cc7c1d
Rais1one <147911504+Rais1one@users.noreply.github.com>
2026-04-09 20:56:35 +0300

    Add files via upload

---
+++ labasp23-24/memory_allocation_1/memory_allocation_1/memory_allocation_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <new>
+#include <iostream> 
+#include <windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Сколько имен хотите ввести: ";
+    int n;
+    std::cin >> n;
+    std::string* p = new (std::nothrow) std::string[n];
+
+    if (p == nullptr) {
+        std::cerr << "Ошибка выделения памяти!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Введите имена:" << std::endl;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> p[i];
+    }
+
+    std::cout << "Все ваши имена:" << std::endl;
+    for (int i = 0; i < n; ++i) {
+        std::cout << p[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] p;
+    return 0;
+}
---
+++ labasp23-24/memory_allocation_2/memory_allocation_2/memory_allocation_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    float* q = new float; 
+
+   
+    *q = -7.5; 
+
+    std::cout << "Значение переменной q: " << std::fixed << std::setprecision(2) << *q << std::endl;
+
+ 
+    delete q; 
+
+    q = nullptr; 
+
+    return 0;
+}
